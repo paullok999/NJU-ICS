0a098a6c76990ae5f41a6268bdc7a2a7a116a17a
tracer-ics2023 <tracer@njuics.org>
2024-02-04 15:38:41 +0800

    >  compile NEMU 221220000 张三 Linux kalokfeng-VirtualBox 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:38:41 up  5:08,  1 user,  load average: 0.30, 0.43, 0.46

--- ics2023/nemu/src/monitor/sdb/sdb.c
+++ ics2023/nemu/src/monitor/sdb/sdb.c
@@ -105,7 +105,7 @@ static int cmd_x(char *args){
   vaddr_t va;
   int res = sscanf(expr,"%x",&va);
   if(res <= 0){
-    printf("Address convert failed:%d\n",res);
+    printf("Address convert failed\n");
     return 1;
   }
   int len = atoi(count);
